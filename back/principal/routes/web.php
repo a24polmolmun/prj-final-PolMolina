d03f530b99a2706c8d6e0635ef2d6678d82e9cfc
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-02-19 12:22:29 +0100

    Add google_id field to Usuari model and update user creation logic for Google authentication

--- back/principal/routes/web.php
+++ back/principal/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Usuari;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
-use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,23 +25,24 @@ Route::get('/auth/callback', function () {
   //Assignació de rol
   $user_rol = 'Alumne';
 
+
   if(preg_match('/^a[0-9]{2}/', $user_email)) {
     $user_rol = 'Profe';
   }
   
   //upCrate per google_id
-  $user = User::updateOrCreate(
+  $user = Usuari::updateOrCreate(
     //preguntar
   ['google_id' => $googleUser->getId()],
   [
     'nom'=> $googleUser->getName(),
     'email' => $user_email,
-    'rol' => $user_rol
-    //No hace falta guardar token de google pq no conectamos a suite de google
+    'rol' => $user_rol,
+    'token' => $googleUser->token
   ]);
 
   Auth::login($user);
 
   //Cambiar to: URL
-  return redirect(`/$user_rol/dashboard`);
+  return redirect("/$user_rol/dashboard");
 });
