ccc918df5c8e89830e405f77397eee2b7a577d4d
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-02-23 07:55:54 +0100

    Afegir component AlumneName i ruta rel·lacionada en les rutes de l'aplicació

---
+++ front/src/app/features/alumnes/[alumneName]/alumneName.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnes',
+  imports: [],
+  templateUrl: './alumneName.component.html',
+  styleUrl: './alumneName.component.css',
+})
+export class AlumneNameComponent {}
