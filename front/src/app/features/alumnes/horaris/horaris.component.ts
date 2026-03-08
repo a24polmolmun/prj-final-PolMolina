491d0374567d9ce3c77e9274e1ebb3b07b7571f1
a22tonmarmar <a22tonmarmar@inspedralbes.cat>
2026-03-08 22:41:10 +0100

    Refactor horaris component: update styles and layout, add sidebar component

--- front/src/app/features/alumnes/horaris/horaris.component.ts
+++ front/src/app/features/alumnes/horaris/horaris.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HorarisManagerService } from '../../../shared/services/horaris/horaris-manager.service';
+import { SidebarAlumneComponent } from '../../../shared/components/sidebar/alumnes/sidebarAlumne.component';
 
 @Component({
   selector: 'horaris-alumne',
-  imports: [],
+  imports: [SidebarAlumneComponent],
   templateUrl: './horaris.component.html',
   styleUrl: './horaris.component.css',
 })
